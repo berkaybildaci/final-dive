e297ce259d9e4809be06fc195a9dc68d4ae04ab7
Anshul <anshulmistry1@gmail.com>
2024-05-12 01:18:34 -0500

    enemy
    Adds the Enemy Script and Movement

--- Assets/Code/Enemy.cs
+++ Assets/Code/Enemy.cs
@@ -30,7 +30,7 @@ public class Enemy : MonoBehaviour
 
     private void Awake()
     {
-        player = GameObject.Find("PlayerObj").transform;
+        player = GameObject.Find("Player").transform;
         agent = GetComponent<NavMeshAgent>();
     }
 
