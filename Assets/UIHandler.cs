69d08e4c7567770b95da6a0963005fc600f7f2d4
berkaybildaci <berkaybildaci@gmail.com>
2024-05-13 13:18:07 -0500

    wip

---
+++ Assets/UIHandler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIHandler : MonoBehaviour
+{
+    public static UIHandler UIHandlerInstance;
+    public 
+    void Awake()
+    {
+        UIHandlerInstance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
