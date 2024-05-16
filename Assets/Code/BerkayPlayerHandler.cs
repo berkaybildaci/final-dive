de5960f9f33126a6544c7cef8f80cfccf9516600
berkaybildaci <berkaybildaci@gmail.com>
2024-05-16 09:08:35 -0500

    polish

--- Assets/Code/BerkayPlayerHandler.cs
+++ Assets/Code/BerkayPlayerHandler.cs
@@ -2,20 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BerkayPlayerHandler : MonoBehaviour
+namespace cakeslice
 {
-    private float pickupRange;
-    [SerializeField] private BerkayGunScript gunScriptInstance;
-    // Start is called before the first frame update
-    void Start()
+    public class BerkayPlayerHandler : MonoBehaviour
     {
-        pickupRange = 5f;
-    }
+        private float pickupRange;
+        [SerializeField] private BerkayGunScript gunScriptInstance;
+        // Start is called before the first frame update
+        void Start()
+        {
+            pickupRange = 5f;
+            //GetComponent<Outline>().enabled = !GetComponent<Outline>().enabled;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.E))
+        // Update is called once per frame
+        void Update()
+        {
+            HighlightItem();
+            PickupItem();
+        }
+
+        void PickupItem()
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                RaycastHit hit;
+                Vector3 forward = Camera.main.transform.forward;
+                Ray ray = new Ray(Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0f)), forward);
+                if (Physics.Raycast(ray, out hit, pickupRange))
+                {
+                    if (hit.collider.gameObject.tag == "Pickup")
+                    {
+                        hit.collider.gameObject.SetActive(false);
+                        gunScriptInstance.pickedUp = true;
+                        Debug.Log("Picked Up Item!");
+                    }
+                }
+            }
+        }
+        void HighlightItem()
         {
             RaycastHit hit;
             Vector3 forward = Camera.main.transform.forward;
@@ -24,11 +49,32 @@ public class BerkayPlayerHandler : MonoBehaviour
             {
                 if (hit.collider.gameObject.tag == "Pickup")
                 {
-                    hit.collider.gameObject.SetActive(false);
-                    gunScriptInstance.pickedUp = true;
-                    Debug.Log("Picked Up Item!");
+                    hit.collider.GetComponent<BerkayGunPickup>().currentlyHighlighted = true;
+                    hit.collider.transform.GetChild(0).GetComponent<Outline>().enabled = true;
+                }
+                else
+                {
+                    ResetHighlights();
+                }
+            }
+            else
+            {
+                ResetHighlights();
+            }
+        }
+
+        void ResetHighlights()
+        {
+            BerkayGunPickup[] gunPickups = FindObjectsOfType<BerkayGunPickup>();
+            foreach (BerkayGunPickup gunPickup in gunPickups)
+            {
+                if (gunPickup.currentlyHighlighted == true)
+                {
+                    gunPickup.gameObject.transform.GetChild(0).GetComponent<Outline>().enabled = false;
+                    gunPickup.currentlyHighlighted = false;
                 }
             }
         }
     }
 }
+
