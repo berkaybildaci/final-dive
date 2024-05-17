c8543ea995467590a87d9926349f02f4ae8d6144
berkaybildaci <berkaybildaci@gmail.com>
2024-05-17 11:16:01 -0500

    wip damage enemy

--- Assets/Code/EntityData.cs
+++ Assets/Code/EntityData.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class EntityData : MonoBehaviour
 {
     public int health;
+    public int maxHealth;
+    public Image healthBarUI;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,10 @@ public class EntityData : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(healthBarUI != null)
+        {
+            healthBarUI.fillAmount = health / maxHealth;
+        }
         if(health <= 0)
         {
             Destroy(gameObject);
