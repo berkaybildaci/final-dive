b4355332d370017e9403db61f69f809cf1684317
Kp101coder <prabhukrish710@gmail.com>
2024-05-17 01:23:35 -0500

    Revert "Merge branch 'main' of https://github.com/berkaybildaci/final-dive"
    This reverts commit bf1d397eef509589b948c5f610ec6402ef4ea91d, reversing
    changes made to ba53660b8449c667acf966b028f0f62206f14c1d.

--- Assets/Code/BerkayGunScript.cs
+++ Assets/Code/BerkayGunScript.cs
@@ -6,9 +6,6 @@ public class BerkayGunScript : MonoBehaviour
 {
     [SerializeField] private GameObject mainCamera;
     public bool pickedUp;
-    [SerializeField] private GameObject bulletPrefab;
-    [SerializeField] private GameObject shotPoint;
-    [SerializeField] private GameObject muzzleFlash;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,16 +18,8 @@ public class BerkayGunScript : MonoBehaviour
         if(pickedUp)
         {
             gameObject.GetComponent<MeshRenderer>().enabled = true;
-            if(Input.GetMouseButtonDown(0))
-            {
-                Shoot();
-            }
         }
-    }
-
-    void Shoot()
-    {
-        GameObject bullet = Instantiate(bulletPrefab, shotPoint.transform.position, shotPoint.transform.rotation);
-        bullet.GetComponent<Rigidbody>().AddForce(shotPoint.transform.forward * bullet.GetComponent<Rigidbody>().mass * 100f);
+        //transform.position = mainCamera.transform.position;
+        //ransform.rotation = mainCamera.transform.rotation;
     }
 }
