2bce26b7730220610780b5b6deae6bb50ac442ef
Moksh Shah <mokshpshah1@gmail.com>
2024-05-19 17:03:50 -0500

    hey

--- Assets/Code/checkWin.cs
+++ Assets/Code/checkWin.cs
@@ -1,23 +1,78 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
-public class checkWin : MonoBehaviour
+public class CheckWin : MonoBehaviour
 {
     public TextMeshProUGUI text;
-    // Start is called before the first frame update
+    public TextMeshProUGUI timer;
+    public Image time;
+
+
+    private float gameTime = 20f; // Total game time in seconds
+    private float currentTime = 0f; // Current time elapsed
+    private bool gameEnded = false; // Flag to track if the game has ended
+
     void Start()
     {
         text.enabled = false;
+        timer.enabled=true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(transform.childCount == 0)
+        if (!gameEnded)
         {
-            text.enabled = true;
+            HandleGameTimer();
+            CheckVictoryConditions();
         }
     }
+
+    private void HandleGameTimer()
+    {
+        currentTime += Time.deltaTime; // Increment the current time by the time elapsed since the last frame
+        float remainingTime = Mathf.Max(gameTime - currentTime, 0f);
+        timer.text = ""+ remainingTime.ToString("F1");
+        time.fillAmount=1-currentTime/gameTime;
+
+
+        // Update UI elements or perform other actions based on the current time
+        if (currentTime >= gameTime)
+        {
+            EndGame();
+        }
+    }
+
+    private void CheckVictoryConditions()
+    {
+        // Check other victory conditions if needed
+    }
+
+    private void EndGame()
+{
+    GameObject[] enemies = GameObject.FindGameObjectsWithTag("EnemyContainer");
+    foreach (GameObject enemy in enemies)
+    {
+        Destroy(enemy);
+    }
+    GameObject[] enemie = GameObject.FindGameObjectsWithTag("TEAM");
+    foreach (GameObject enemy in enemie)
+    {
+        Destroy(enemy);
+    }
+    GameObject[] enemi = GameObject.FindGameObjectsWithTag("spawner");
+    foreach (GameObject enemy in enemi)
+    {
+        Destroy(enemy);
+    }
+    
+
+    // Update UI to display win message
+    timer.enabled=false;
+    text.enabled = true;
+    text.text = "You Win!";
+
+  
+
+}
 }
