c2bc7df5f1fac363a058880ecbb41c8828d06684
Moksh Shah <mokshpshah1@gmail.com>
2024-05-19 22:49:51 -0500

    d

---
+++ Assets/GoBack.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GoBack : MonoBehaviour
+{
+    public void Controls() {
+        SceneManager.LoadSceneAsync("Intro");
+    }
+}
